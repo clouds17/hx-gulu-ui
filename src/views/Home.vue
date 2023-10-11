b93bcbf5136809c569f535968e14a11490419199
黄鑫 <990695619@qq.com>
2023-10-11 17:52:53 +0800

    初始化aside的html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
             <h1>小黄VUE3造轮子 </h1>
             <h2>自己做得一个UI框架</h2>
             <p class="actions">
-                <a href="">GitHub</a>
-                <a href="">开始</a>
+                <a href="https://github.com/clouds17/hx-gulu-ui" target="_blank">GitHub</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
@@ -22,5 +22,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions {
+        padding: 8px 0;
+        a {
+            margin: 0 8px;
+            background: #fff;
+            display: inline-block;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h / 2;
+            padding: 0 8px;
+        }
+    }
+}
 </style>
